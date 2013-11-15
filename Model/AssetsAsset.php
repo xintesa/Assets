0c4c6ded929e3a49c93bf3e4b7db4678694c2c43
Rachman Chavik <rchavik@xintesa.com>
2013-11-15 19:03:48 +0700

    Make AssetUsage dependent

--- Model/AssetsAsset.php
+++ Model/AssetsAsset.php
@@ -14,6 +14,7 @@ class AssetsAsset extends AssetsAppModel {
 	public $hasMany = array(
 		'AssetsAssetUsage' => array(
 			'className' => 'Assets.AssetsAssetUsage',
+			'dependent' => true,
 		),
 	);
 
