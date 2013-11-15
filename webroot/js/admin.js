1bd3d080b818c92612abf1d99bf187af4e3a9f2e
Rachman Chavik <rchavik@xintesa.com>
2013-11-15 21:08:55 +0700

    Enhance popup centering code

--- webroot/js/admin.js
+++ webroot/js/admin.js
@@ -1,9 +1,17 @@
 Assets = {}
 
 Assets.popup = function(e) {
+	var width = 800;
+	var height = 600;
+	var screenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+	var screenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+	var left = (screen.width / 2) - (width / 2) + screenLeft;
+	var top = (screen.height / 4) - (height / 4) + screenTop;
 	var url = e.currentTarget.attributes['href'].value;
-	var options = 'menubar=no,resizable=yes,height=600,width=800,chrome=yes,centerScreen=yes,scrollbars=yes';
-	this.win = window.open(url, 'Asset Browser', options).focus();
+	var options = 'menubar=no,resizable=yes,chrome=yes,centerScreen=yes,scrollbars=yes' +
+		',top=' + top + ',left=' + left +
+		',width=' + width + ',height=' + height;
+	window.open(url, 'Asset Browser', options).focus();
 	e.preventDefault();
 	return false;
 }
