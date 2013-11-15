1bf30f1b08f3fea6c7124b4fdd94931f4adeee7e
Rachman Chavik <rchavik@xintesa.com>
2013-11-15 16:32:51 +0700

    Focus popup when already opened

--- webroot/js/admin.js
+++ webroot/js/admin.js
@@ -3,7 +3,7 @@ Assets = {}
 Assets.popup = function(e) {
 	var url = e.currentTarget.attributes['href'].value;
 	var options = 'menubar=no,resizable=yes,height=600,width=800,chrome=yes,centerScreen=yes,scrollbars=yes';
-	this.win = window.open(url, 'Asset Browser', options);
+	this.win = window.open(url, 'Asset Browser', options).focus();
 	e.preventDefault();
 	return false;
 }
