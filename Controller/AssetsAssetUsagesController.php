a58dc42f6909db5d8d5226ea7fb7b8745b06c334
Wiena Lorentz <lorentz@xintesa.com>
2016-04-15 19:48:48 +0700

    Use query() to avoid undefined index error

--- Controller/AssetsAssetUsagesController.php
+++ Controller/AssetsAssetUsagesController.php
@@ -23,12 +23,10 @@ class AssetsAssetUsagesController extends AssetsAppController {
 	public function admin_add() {
 		if (isset($this->request->query)) {
 			$assetId = $model = $foreignKey = $type = null;
-			$assetId = $this->request->query['asset_id'];
-			$model = $this->request->query['model'];
-			$foreignKey = $this->request->query['foreign_key'];
-			if (isset($this->request->query['type'])) {
-				$type = $this->request->query['type'];
-			}
+			$assetId = $this->request->query('asset_id');
+			$model = $this->request->query('model');
+			$foreignKey = $this->request->query('foreign_key');
+			$type = $this->request->query('type');
 
 			$conditions = array(
 				'asset_id' => $assetId,
